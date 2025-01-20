709d5459616d62118249ec3860ada268be3e4aa2
Toaster_Machine <jeremy.villanova@orange.fr>
2025-01-20 15:09:12 +0100

    first commit

---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { ref } from 'vue';
+
+export default{
+    name:'HelloWorld',
+    props:{
+        count:{
+            type: Number,
+            default: 2
+        },
+    },
+    setup(props) {
+        const message = ref('Bonjour tout le monde')
+        const countData = props.count
+        return {
+            message,
+            countData
+        }
+    },
+}
+</script>
+
+<template>
+    <div>{{ message }} {{ countData }}</div>
+</template>
+<style></style>
---
+++ src/views/ProductList.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+
+interface Product {
+    id: number
+    label: string
+    description: string
+    quantity: number
+}
+
+defineProps<{ products: Product[] }>()
+
+const getStatus = (quantity: number): String => {
+    return quantity > 0 ? 'Disponible' : 'Indisponible'
+}
+</script>
+
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Label</th>
+                <th>Description</th>
+                <th>Statut</th>
+            </tr>
+        </thead>
+        <body>
+            <tr v-for="product in products" :key="product.id">
+                <td>{{ product.id }}</td>
+                <td>{{ product.label }}</td>
+                <td>{{ product.description }}</td>
+                <td>{{ getStatus(product.quantity) }}</td>
+            </tr>
+        </body>
+    </table>
+</template>
